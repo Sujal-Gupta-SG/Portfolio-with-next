f7e17199f9cf9745e263457cbfe1a672795bdeac
Sujal-Gupta-SG <Sujal24092004+github@gmail.com>
2024-10-04 22:23:10 +0530

    update

--- src/app/data.js
+++ src/app/data.js
@@ -19,61 +19,152 @@ Websites:
 export const projectsData = [
   {
     id: 1,
-    name: "Calculator",
-    description: "This HTML document represents a calculator with basic arithmetic functions and additional trigonometric, logarithmic, and factorial operations. The associated CSS styles provide a visually appealing interface. The JavaScript functionality allows input through both button clicks and keyboard events, enhancing user interaction.",
-    demoLink: "https://sujal-gupta-sg.github.io/calculator/",
+    name: "<PassOp/> : Secure Password Manager",
+    description:
+      "Password Manager App using the MERN stack (MongoDB, Express, React, Node.js), focused on security and user experience. The app allows users to securely store and manage their passwords, with each password encrypted before being stored in the database.",
+    demoLink: "https://passward-manager.onrender.com/",
   },
   {
     id: 2,
-    name: "Tic Tac Toe",
-    description: "This HTML document defines a responsive Tic-Tac-Toe game with player name input and a visually appealing 404 error page. The game uses JavaScript for interactivity, featuring dynamic player turns and a winning pattern display.",
-    demoLink: "https://sujal-gupta-sg.github.io/Tictactoe/",
+    name: "Stock Management System for Efficient Inventory Control",
+    description:
+      "Stock Management System that simplifies and automates the process of managing product inventory, ensuring smooth operations for businesses of all sizes. The system provides essential features for streamlined inventory control.",
+    demoLink: "https://stock-manager-gq5o.onrender.com/",
   },
   {
     id: 3,
-    name: "Bank Management System",
-    description: "The system allows users to perform various banking operations, including opening a new account and viewing account details. Users must enter a password to access the system.",
-    demoLink: "https://github.com/Sujal-Gupta-SG/c_project_simple_bank_management_system",
+    name: "StayHub: A Comprehensive Property Listing Platform",
+    description:
+      "StayHub is a dynamic web application for property listings, allowing users to create, view, and review properties.",
+    demoLink: "https://stayhub-uoq2.onrender.com/listings",
   },
   {
     id: 4,
-    name: "Library Management System",
-    description: "This Python program represents a basic library management system, allowing users to display available books, lend books, and return books.",
-    demoLink: "https://github.com/Sujal-Gupta-SG/python-programs/tree/main/book%20library",
+    name: "Bank Transaction System",
+    description:
+      "This project is a simple bank transaction system built using Node.js, Express, and MySQL for database operations. It allows users to view customer details, transfer money between customers, and view transaction success details.",
+    demoLink: "https://github.com/Sujal-Gupta-SG/Bank-Transaction-System",
   },
   {
     id: 5,
-    name: "Railway Reservation System",
-    description: "This C++ program implements a simple Railway Reservation System that allows users to book, cancel, and view their train tickets.",
-    demoLink: "https://github.com/Sujal-Gupta-SG/cpp_project/tree/main/railway%20demo",
+    name: "Book Library",
+    description:
+      "A simple book library management system. This program allows users to add, remove, and search for books in a virtual library. It includes basic features for organizing and managing book information.",
+    demoLink: "https://github.com/Sujal-Gupta-SG/python-programs",
   },
   {
     id: 6,
-    name: "Simple C++ Time Tracker",
-    description: "This C++ program is a basic time tracker that allows users to input hours, minutes, and seconds, displaying and updating the time in a loop.",
-    demoLink: "https://github.com/Sujal-Gupta-SG/cpp_project/tree/main/Small%20clock",
+    name: "Calculator",
+    description:
+      "A basic calculator program that performs arithmetic operations. It provides a simple command-line interface for users to input mathematical expressions and get the results.",
+    demoLink: "https://github.com/Sujal-Gupta-SG/python-programs",
   },
   {
     id: 7,
-    name: "Simple Calculator",
-    description: "This is a simple command-line calculator program written in Python that allows for basic arithmetic operations.",
-    demoLink: "https://github.com/Sujal-Gupta-SG/python-programs/tree/main/calculator",
+    name: "Calculator Website",
+    description:
+      "A simple calculator web application with basic arithmetic and scientific functions. Features include:\n- Addition, subtraction, multiplication, and division.\n- Trigonometric functions: sin, cos, tan.\n- Exponential and logarithmic functions.\n- Factorial, square root, and power functions.\n- Constants: π (pi) and e (Euler's number).",
+    demoLink: "https://sujal-gupta-sg.github.io/calculator/",
   },
   {
     id: 8,
-    name: "Email Validation Tool",
-    description: "A Python project designed to verify the accuracy and validity of email addresses, enhancing data accuracy and reducing errors.",
-    demoLink: "https://github.com/Sujal-Gupta-SG/python-programs/tree/main/email%20validation%20tool",
+    name: "Digital Clock",
+    description:
+      "Display the current time in a digital clock format with hours, minutes, and seconds.",
+    demoLink: "https://github.com/Sujal-Gupta-SG/C_PROJECTS",
   },
   {
     id: 9,
-    name: "Dynamic QR Code Generator",
-    description: "A Python script that allows users to create custom QR codes with ease, simplifying the process of sharing information.",
-    demoLink: "https://github.com/Sujal-Gupta-SG/python-programs/tree/main/QR%20code%20maker",
+    name: "Email Validator",
+    description:
+      "This Python script validates email addresses based on common criteria. It checks for the correct format and ensures that the email follows standard conventions. Useful for input validation in various applications.",
+    demoLink: "https://github.com/Sujal-Gupta-SG/python-programs",
+  },
+  {
+    id: 10,
+    name: "Kaun Banega Crorepati Game",
+    description:
+      "Experience the Indian version of the 'Who Wants to Be a Millionaire?' game, complete with multiple-choice questions, lifelines, and prize money.",
+    demoLink: "https://github.com/Sujal-Gupta-SG/C_PROJECTS",
+  },
+  {
+    id: 11,
+    name: "Number Guessing Game",
+    description:
+      "Guess the randomly generated number within a specified range and a limited number of attempts.",
+    demoLink: "https://github.com/Sujal-Gupta-SG/C_PROJECTS",
+  },
+  {
+    id: 12,
+    name: "Progress Bar",
+    description:
+      "Visualize progress or completion status using a text-based progress bar.",
+    demoLink: "https://github.com/Sujal-Gupta-SG/C_PROJECTS",
+  },
+  {
+    id: 13,
+    name: "QR Code Generator",
+    description:
+      "This Python script generates QR codes. It uses the qrcode library to create QR codes from text or URLs. You can easily customize the content and appearance of the generated QR codes.",
+    demoLink: "https://github.com/Sujal-Gupta-SG/python-programs",
+  },
+  {
+    id: 14,
+    name: "Railway Reservation System",
+    description:
+      "This is a railway reservation system implemented in C++. It allows users to reserve tickets, cancel reservations, and perform various other functions related to railway management.\n\nFeatures:\n- Admin Mode: Administrators can manage the database, add details, and perform user management tasks.\n- User Mode: Users can log in, reserve tickets, cancel reservations, and make inquiries.\n- Database Management: Admins can create and manage user IDs and other database details.\n- Reservation: Users can reserve tickets specifying train details, boarding points, destination, number of seats, etc.\n- Cancellation: Users can cancel reservations with appropriate refund calculations.\n- Enquiry: Users can make inquiries about train details.",
+    demoLink: "https://github.com/Sujal-Gupta-SG/cpp_project",
+  },
+  {
+    id: 15,
+    name: "Simple C++ Time Tracker",
+    description:
+      "This C++ program is a basic time tracker that allows users to input hours, minutes, and seconds. The program then displays and updates the time in a loop.",
+    demoLink: "https://github.com/Sujal-Gupta-SG/cpp_project",
+  },
+  {
+    id: 16,
+    name: "Simple Calculator",
+    description:
+      "Perform basic arithmetic operations such as addition, subtraction, multiplication, and division.",
+    demoLink: "https://github.com/Sujal-Gupta-SG/C_PROJECTS",
+  },
+  {
+    id: 17,
+    name: "Snake Game",
+    description:
+      "Classic Snake game where the player controls a snake to eat food and grow longer without hitting walls or its own tail.",
+    demoLink: "https://github.com/Sujal-Gupta-SG/C_PROJECTS",
+  },
+  {
+    id: 18,
+    name: "Sudoku Solver",
+    description:
+      "Solve Sudoku puzzles automatically using the backtracking algorithm to fill in the missing numbers.",
+    demoLink: "https://github.com/Sujal-Gupta-SG/C_PROJECTS",
+  },
+  {
+    id: 19,
+    name: "TIC TAC TOE",
+    description:
+      "Responsive tic tac toe game for gamers who love to play tic tac toe with a scorecard.",
+    demoLink: "https://sujal-gupta-sg.github.io/Tictactoe/",
+  },
+  {
+    id: 20,
+    name: "Tic-Tac-Toe Game",
+    description:
+      "Play the classic Tic-Tac-Toe game against the computer or another player.",
+    demoLink: "https://github.com/Sujal-Gupta-SG/C_PROJECTS",
+  },
+  {
+    id: 21,
+    name: "User Management System",
+    description:
+      "Manage user accounts with functionalities like account creation, login, logout, and user data display.",
+    demoLink: "https://github.com/Sujal-Gupta-SG/C_PROJECTS",
   },
 ];
-
-
 
 export const BtnList = [
   { label: "Home", link: "/", icon: "home", newTab: false },
@@ -92,7 +183,7 @@ export const BtnList = [
     icon: "linkedin",
     newTab: true,
   },
-  
+
   {
     label: "Resume",
     link: "/Resume.pdf",
